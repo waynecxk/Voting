e0ca3c90bdaffdcada2425e60c73a596917fb31e
unknown <xiankang.chua@outlook.com>
2025-03-08 11:18:12 +0800

    initial commit

--- Voting/voting-app/scripts/deploy.js
+++ Voting/voting-app/scripts/deploy.js
@@ -15,18 +15,7 @@ async function main() {
 
     console.log("Voting contract deployed to:", voting.address);
 
-    // const Voting = await ethers.getContractFactory("Voting");
-    // const deployedContract = await ethers.deployContract("Voting", ["Party A", "Party B", "Party C"]);
-    // console.log("Voting contract deployed to:", deployedContract.address);
-    // const partyNames = ["Party A", "Party B", "Party C"]; // List your parties here
-    // const voting = await Voting.deploy(partyNames);
-    // console.log("Voting contract deployed to:", deployedContract.address);
 }
-
-// Run npx hardhat node in your terminal. Leave the process running. 
-// "npx hardhat node" starts a local Ethereum network with 20 accounts, each with 10000 ETH. You can use these accounts to deploy and interact with your smart contracts.
-// Open a new terminal window.
-// Run npx hardhat run [script-name] --network localhost
 
 main()
     .then(() => process.exit(0))
